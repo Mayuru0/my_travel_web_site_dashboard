554a11515752acb7847969982688fff3b202c57a
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2025-08-04 12:48:38 +0530

    env set

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3dAW4RatDqhWR-7rgCld1eyIXTXC_UgM",
-  authDomain: "mytravelwebsite-d5170.firebaseapp.com",
-  projectId: "mytravelwebsite-d5170",
-  storageBucket: "mytravelwebsite-d5170.firebasestorage.app",
-  messagingSenderId: "324638234234",
-  appId: "1:324638234234:web:fce304857ddedbd2f40dc3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
